8a9e825042318ccb08a444ff4edce78b041f5aa3
Nerio <nrincon.mr@gmail.com>
2019-07-11 14:58:18 -0500

    Model creation

--- masivian_tst/api/models.py
+++ masivian_tst/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class BinaryTree(models.Model):
     name = models.CharField(max_length=50)
-    root = models.CharField(max_length=50)
+    tree = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
