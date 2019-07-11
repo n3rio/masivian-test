7f0b5b0040c575418aeea971c8a3a1d6f18ee96f
Nerio <nrincon.mr@gmail.com>
2019-07-11 14:58:43 -0500

    View creation

--- masivian_tst/api/views.py
+++ masivian_tst/api/views.py
@@ -1,11 +1,34 @@
-from rest_framework import viewsets
+import random
 
+from binarytree import build
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import BinaryTree
 from .serializers import BinaryTreeSerializer
 
-# Create your views here.
+
+def create_tree():
+    values = [random.randint(1, 20) for _ in range(20)]
+    root = build(values)
+    return root, ', '.join(str(e) for e in values)
 
 
 class BinaryTreeViewSet(viewsets.ModelViewSet):
     queryset = BinaryTree.objects.all()
     serializer_class = BinaryTreeSerializer
+
+    def create(self, request, *args, **kwargs):
+        _, tree = create_tree()
+        data = dict(request.data)
+        if isinstance(data['name'], list):
+            name = data['name'][0]
+        data['name'] = name
+        data['tree'] = tree
+        serializer = self.get_serializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            self.perform_create(serializer)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                serializer.data, status=status.HTTP_400_BAD_REQUEST)
